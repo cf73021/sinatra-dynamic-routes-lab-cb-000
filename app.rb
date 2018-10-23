ced0183a9643939a12dbb8a793d36e68df448164
Chris Franklin <cf73021@gmail.com>
2018-10-23 20:34:30 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -10,4 +10,9 @@ class App < Sinatra::Base
   get "/square/:number" do
     (params[:number].to_i ** 2).to_s
   end
+
+  get "/say/:number/:phrase" do
+    params[:number].to_i.times do
+      params[:phrase]
+  end
 end
