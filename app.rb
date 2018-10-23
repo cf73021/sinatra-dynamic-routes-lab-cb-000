890ebc02d7162242d9509ab9b3af09f3d0d5d21a
Chris Franklin <cf73021@gmail.com>
2018-10-23 20:38:34 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -18,4 +18,8 @@ class App < Sinatra::Base
     end
     @answer
   end
+
+  get "/say/:word1/:word2/:word3/:word4/:word5" do
+    "#{:word1} + " " + #{:word2} + " " + #{:word3} + " " + #{:word4} + " " + #{:word5} + " ""
+  end
 end
