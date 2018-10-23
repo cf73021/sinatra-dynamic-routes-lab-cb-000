beca38c2c42cb170f0672de92356c84bc820b362
Chris Franklin <cf73021@gmail.com>
2018-10-23 20:41:10 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -20,6 +20,6 @@ class App < Sinatra::Base
   end
 
   get "/say/:word1/:word2/:word3/:word4/:word5" do
-    "#{params[:word1]} + " " + #{:word2} + " " + #{:word3} + " " + #{:word4} + " " + #{:word5} + "."" 
+    "#{params[:word1]} + " " + #{params[:word2]} + " " + #{:word3} + " " + #{:word4} + " " + #{:word5} + "."" 
   end
 end
